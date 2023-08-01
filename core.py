efae97921424efdd9119979723b048c514015042
OperatoR-R <111383899+walltrone@users.noreply.github.com>
2023-08-01 13:00:19 +0400

    Add files via upload

--- core.py
+++ core.py
@@ -1,4 +1,4 @@
-from datetime import datetime 
+from datetime import datetime
 
 import re
 
@@ -7,29 +7,37 @@ import vk_api
 from config import acces_token
 
 
-
 class VkTools():
     def __init__(self, acces_token):
-       self.api = vk_api.VkApi(token=acces_token)
-       self.offset = 0
+        self.api = vk_api.VkApi(token=acces_token)
+        self.offset = 0
 
     def get_profile_info(self, user_id):
 
         info, = self.api.method('users.get',
-                            {'user_id': user_id,
-                            'fields': 'city,bdate,sex,relation,home_town' 
-                            }
-                            )
-        user_info = {'name': info['first_name'] + ' '+ info['last_name'],
+                                {'user_id': user_id,
+                                 'fields': 'city,bdate,sex,relation,home_town'
+                                 }
+                                )
+        user_info = {'name': info['first_name'] + ' ' + info['last_name'],
                      'id':  info['id'],
                      'bdate': info['bdate'] if re.search(r'\d+\.\d+\.\d{4}', info['bdate']) else None,
-                     'home_town': info['home_town'] if 'home_town' in info else None,
                      'sex': info['sex'] or None,
-                     'city': info['city']['id'],
-                     'relation': info['relation'] or None
+                     'city': info['city']['id'] if 'city' in info else None,
+                     'relation': info['relation'] or None,
+                     'offset': 0
                      }
         return user_info
-    
+
+    def get_city_id(self, city_name):
+        cities = self.api.method('database.getCities',
+                                 {'q': city_name,
+                                  'count': 1,
+                                  'need_all': 1
+                                  }
+                                 )
+        return cities['items'][0]['id']
+
     def search_users(self, params):
 
         sex = params['sex'] % 2 + 1
@@ -37,35 +45,35 @@ class VkTools():
         curent_year = datetime.now().year
         user_year = int(params['bdate'].split('.')[2])
         age = curent_year - user_year
-        age_from = age - 5
-        age_to = age + 5
+        age_from = age - 5 if age - 5 > 0 else age
+        age_to = age + 5 if age + 5 < 120 else 120
+        offset = params['offset']
 
         users = self.api.method('users.search',
                                 {'count': 10,
-                                 'offset': self.offset,
+                                 'offset': offset,
                                  'age_from': age_from,
                                  'age_to': age_to,
                                  'sex': sex,
                                  'city': city,
                                  'status': 6,
                                  'is_closed': False
-                                }
-                            )
-        self.offset += 10
+                                 }
+                                )
         try:
             users = users['items']
         except KeyError:
             return []
-        
+
         res = []
 
         for user in users:
             if user['is_closed'] == False:
-                res.append({'id' : user['id'],
+                res.append({'id': user['id'],
                             'name': user['first_name'] + ' ' + user['last_name']
-                           }
+                            }
                            )
-        
+
         return res
 
     def get_photos(self, user_id):
@@ -73,13 +81,13 @@ class VkTools():
                                  {'user_id': user_id,
                                   'album_id': 'profile',
                                   'extended': 1
-                                 }
-                                )
+                                  }
+                                 )
         try:
             photos = photos['items']
         except KeyError:
             return []
-        
+
         res = []
 
         for photo in photos:
@@ -88,8 +96,8 @@ class VkTools():
                         'likes': photo['likes']['count'],
                         'comments': photo['comments']['count'],
                         }
-                        )
-            
+                       )
+
         res.sort(key=lambda x: x['likes']+x['comments']*10, reverse=True)
 
         return res
@@ -97,7 +105,6 @@ class VkTools():
 
 if __name__ == '__main__':
     bot = VkTools(acces_token)
-    params = bot.get_profile_info(789657038)
+    params = bot.get_profile_info()
     users = bot.search_users(params)
     print(bot.get_photos(users[2]['id']))
-
--- interface.py
+++ interface.py
@@ -1,4 +1,6 @@
 # импорты
+from datetime import datetime
+
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.utils import get_random_id
@@ -12,8 +14,8 @@ class BotInterface():
     def __init__(self, comunity_token, acces_token):
         self.interface = vk_api.VkApi(token=comunity_token)
         self.api = VkTools(acces_token)
-        self.params = None
-        self.users = []
+        self.params = dict()
+        self.users = dict()
 
     def message_send(self, user_id, message, attachment=None):
         self.interface.method('messages.send',
@@ -33,50 +35,58 @@ class BotInterface():
                 command = event.text.lower()
 
                 if command == 'привет':
-                    if self.params is None:
-                        self.params = self.api.get_profile_info(event.user_id)
+                    if self.params.get(event.user_id) is None:
+                        self.params[event.user_id] = self.api.get_profile_info(
+                            event.user_id)
                         empty_data = [
-                            key for key, value in self.params.items() if value is None]
+                            key for key, value in self.params[event.user_id].items() if value is None]
                     self.message_send(
-                        event.user_id, f'Здравствуй {self.params["name"]}')
+                        event.user_id, f'Здравствуй {self.params[event.user_id]["name"]}')
                     if empty_data:
                         data = {
-                            'bdate': 'год рождения (1901-2022)',
-                            'home_town': 'город',
+                            'bdate': 'год рождения (например: 2000)',
                             'sex': 'пол (м/ж)',
                             'relation': 'семейное положение',
+                            'city': 'город'
                         }
                         key = empty_data.pop()
                         self.message_send(
                             event.user_id, f'Пожалуйста, назовите {data[key]}')
 
-                elif self.params is not None and self.params[key] is None:
+                elif self.params.get(event.user_id) is not None and self.params[event.user_id][key] is None:
                     match key, command:
                         case 'bdate', command:
-                            if command.isdigit() and 2023 > int(command) > 1900:
-                                self.params[key] = f'0.0.{command}'
+                            if command.isdigit() and datetime.now().year > int(command) > 1901:
+                                self.params[event.user_id][key] = f'0.0.{command}'
                                 self.message_send(event.user_id, 'Принято!')
                             else:
                                 self.message_send(
                                     event.user_id, 'неверное значение, попробуйте еще раз')
                         case 'sex', ('м' | 'ж'):
-                            self.params[key] = {'м': 2, 'ж': 1}[command]
+                            self.params[event.user_id][key] = {
+                                'м': 2, 'ж': 1}[command]
                             self.message_send(event.user_id, 'Принято!')
-                        case ('relation' | 'home_town'), command:
-                            self.params[key] = command
+                        case 'relation', command:
+                            self.params[event.user_id][key] = command
+                            self.message_send(event.user_id, 'Принято!')
+                        case 'city', command:
+                            self.params[event.user_id][key] = self.api.get_city_id(
+                                command)
                             self.message_send(event.user_id, 'Принято!')
                         case _:
                             self.message_send(
                                 event.user_id, 'неверное значение, попробуйте еще раз')
-                    if self.params[key] is not None and empty_data:
+                    if self.params[event.user_id][key] is not None and empty_data:
                         key = empty_data.pop()
                         self.message_send(
                             event.user_id, f'Пожалуйста, назовите {data[key]}')
                 else:
-                    if self.params is not None and command == 'поиск':
-                        if self.users == []:
-                            self.users = self.api.search_users(self.params)
-                        user = self.users.pop()
+                    if self.params.get(event.user_id) is not None and command == 'поиск':
+                        if self.users.setdefault(event.user_id, []) == []:
+                            self.users[event.user_id] = self.api.search_users(
+                                self.params[event.user_id])
+                            self.params[event.user_id]['offset'] += 10
+                        user = self.users[event.user_id].pop()
 
                         # здесь логика дял проверки бд
                         photos_user = self.api.get_photos(user['id'])
